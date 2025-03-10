94ae922dceea9d8fe4aa03d7ad73268bfb0b5d32
harshnugget <76251920+harshnugget@users.noreply.github.com>
2025-03-10 08:20:46 +0000

    feat(Form.jsx): Created form template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import Form from "./components/Form";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Form />
+    </>
+  );
 }
 
 export default App;
