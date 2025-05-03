bbe22bc277cc4506556a274374e0608987026d86
harshnugget <76251920+harshnugget@users.noreply.github.com>
2025-05-03 11:46:53 +0100

    refactor(App.jsx, main.jsx): Updates to reflect changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import CV from "./components/CV";
+import Page from "./new-feature/Page";
 // import "./App.css";
 
 function App() {
-  const [data, setData] = useState({
-    contactInfo: {
-      firstName: "",
-      lastName: "",
-      phone: "",
-      email: "",
-    },
-    profile: "",
-    jobs: [],
-    educations: [],
-    skills: [],
-  });
-
-  const updateData = (field, value) => {
-    setData((prev) => ({
-      ...prev,
-      [field]: value,
-    }));
-  };
-
   return (
     <>
-      <Form formData={data} updateFormData={updateData} />
-      <CV cvData={data} />
+      {/* <Form formData={data} updateFormData={updateData} />
+      <CV cvData={data} /> */}
+      <Page />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Page from "./new-feature/Page.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <Page />
+    <App />
   </StrictMode>
 );
