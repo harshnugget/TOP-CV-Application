d9d7fac9b9c2b675b514235e29a22faca78ac31b
harshnugget <76251920+harshnugget@users.noreply.github.com>
2025-05-09 15:32:49 +0100

    refactor(Page.jsx): Added page container, styles and additional changes
    - Added a page container for better css styling

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -23,9 +23,17 @@ export default function Page({ gridAreaLayout = defaultGrid }) {
     education: [],
   });
 
-  const width = 560;
+  const width = 750;
   const height = width * 1.41;
   const pageInset = Math.round(width / 30);
+
+  const containerStyles = {
+    border: "solid black 1px",
+    padding: `${pageInset + 1}px`,
+    boxShadow: `inset 0 0 0 ${pageInset}px white, inset 0 0 0 ${
+      pageInset + 1
+    }px grey`,
+  };
 
   const styles = {
     boxSizing: "border-box",
@@ -34,14 +42,10 @@ export default function Page({ gridAreaLayout = defaultGrid }) {
     gridTemplateAreas: gridAreaLayout,
     gridAutoRows: "max-content",
     width: `${width}px`,
+    minWidth: `${width}px`,
     maxWidth: "100%",
     minHeight: `${height}px`,
     backgroundColor: "white",
-    border: "solid black 1px",
-    padding: `${pageInset + 1}px`,
-    boxShadow: `inset 0 0 0 ${pageInset}px white, inset 0 0 0 ${
-      pageInset + 1
-    }px grey`,
   };
 
   function onClickPreviewBtn() {
@@ -55,54 +59,63 @@ export default function Page({ gridAreaLayout = defaultGrid }) {
   return (
     <>
       <div
-        className={`cv-page ${previewMode ? "preview-mode" : "edit-mode"}`}
-        style={{ ...styles }}
+        className={`cv-page-container ${
+          previewMode ? "preview-mode" : "edit-mode"
+        }`}
+        style={{ ...containerStyles }}
       >
-        <ContactDetails
-          updateContactDetails={(contactDetails) =>
-            setData({ ...data, ...contactDetails })
-          }
-          previewMode={previewMode}
-          style={{ gridArea: "contact-details" }}
-        />
-        <div className="skills-header" style={{ gridArea: "skills-header" }}>
-          Skills
-        </div>
-        <div className="skills-content" style={{ gridArea: "skills-content" }}>
-          <Skills
-            updateSkills={(skills) => setData({ ...data, skills })}
+        <div className="cv-page" style={{ ...styles }}>
+          <ContactDetails
+            updateContactDetails={(contactDetails) =>
+              setData({ ...data, ...contactDetails })
+            }
             previewMode={previewMode}
-            style={{ display: "block", gridArea: "skills-content" }}
+            style={{ gridArea: "contact-details" }}
           />
-        </div>
-        <div
-          className="experience-header"
-          style={{ gridArea: "experience-header" }}
-        >
-          Experience
-        </div>
-        <div
-          className="experience-content"
-          style={{ gridArea: "experience-content" }}
-        >
-          <Experience
-            updateExperience={(experience) => setData({ ...data, experience })}
-            previewMode={previewMode}
-            style={{ display: "block", gridArea: "experience-content" }}
-          />
-        </div>
-        <div
-          className="education-header"
-          style={{ display: "block", gridArea: "education-header" }}
-        >
-          Education
-        </div>
-        <div className="education-content">
-          <Education
-            updateEducation={(education) => setData({ ...data, education })}
-            previewMode={previewMode}
-            style={{ display: "block", gridArea: "education-content" }}
-          />
+          <div className="skills-header" style={{ gridArea: "skills-header" }}>
+            Skills
+          </div>
+          <div
+            className="skills-content"
+            style={{ gridArea: "skills-content" }}
+          >
+            <Skills
+              updateSkills={(skills) => setData({ ...data, skills })}
+              previewMode={previewMode}
+              style={{ display: "block", gridArea: "skills-content" }}
+            />
+          </div>
+          <div
+            className="experience-header"
+            style={{ gridArea: "experience-header" }}
+          >
+            Experience
+          </div>
+          <div
+            className="experience-content"
+            style={{ gridArea: "experience-content" }}
+          >
+            <Experience
+              updateExperience={(experience) =>
+                setData({ ...data, experience })
+              }
+              previewMode={previewMode}
+              style={{ display: "block", gridArea: "experience-content" }}
+            />
+          </div>
+          <div
+            className="education-header"
+            style={{ display: "block", gridArea: "education-header" }}
+          >
+            Education
+          </div>
+          <div className="education-content">
+            <Education
+              updateEducation={(education) => setData({ ...data, education })}
+              previewMode={previewMode}
+              style={{ display: "block", gridArea: "education-content" }}
+            />
+          </div>
         </div>
       </div>
       {previewMode ? (
